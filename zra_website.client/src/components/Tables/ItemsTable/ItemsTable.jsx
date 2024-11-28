bc5f62ef837c38fd8ce992133323208d12888efc
Hadi Cheaito <cheaitohadi97@gmail.com>
2024-11-28 12:39:10 +0200

    Getting Items for Items table

--- zra_website.client/src/components/Tables/ItemsTable/ItemsTable.jsx
+++ zra_website.client/src/components/Tables/ItemsTable/ItemsTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './ItemsTable.css';
 import {
     MagnifyingGlassIcon,
@@ -14,47 +14,55 @@ import {
     CardFooter,
     Input,
 } from "@material-tailwind/react";
+import { requestHandler } from "../../../api/axios.js";
 
 const ROWS_PER_PAGE = 6;
 const TABLE_HEAD = ["Declaration No.", "Quantity", "HS Code", "Item Name", "Status", "Date"];
 
-const TABLE_ROWS = [
-    { declaration: "C 59637-2024-NKA", quantity: "150.0000", hsCode: "40112011", itemName: "275/70R22.5 (POWERTRAC HIGHWAY TIRES)", status: "Pending", date: "20240825" },
-    { declaration: "C 59637-2024-NKB", quantity: "300.5000", hsCode: "40112012", itemName: "295/75R22.5 (POWERTRAC OFFROAD TIRES)", status: "Uploaded", date: "20240826" },
-    { declaration: "C 59637-2024-NKC", quantity: "200.2500", hsCode: "40112013", itemName: "385/65R22.5 (POWERTRAC TRUCK TIRES)", status: "Uploaded", date: "20240827" },
-    { declaration: "C 59637-2024-NKD", quantity: "120.0000", hsCode: "40112014", itemName: "225/75R16 (POWERTRAC SUV TIRES)", status: "Canceled", date: "20240828" },
-    { declaration: "C 59637-2024-NKr", quantity: "150.0000", hsCode: "40112011", itemName: "275/70R22.5 (POWERTRAC HIGHWAY TIRES)", status: "Pending", date: "20240825" },
-    { declaration: "C 59637-2024-NKt", quantity: "300.5000", hsCode: "40112012", itemName: "295/75R22.5 (POWERTRAC OFFROAD TIRES)", status: "Uploaded", date: "20240826" },
-    { declaration: "C 59637-2024-NKy", quantity: "200.2500", hsCode: "40112013", itemName: "385/65R22.5 (POWERTRAC TRUCK TIRES)", status: "Uploaded", date: "20240827" },
-    { declaration: "C 59637-2024-NKh", quantity: "120.0000", hsCode: "40112014", itemName: "225/75R16 (POWERTRAC SUV TIRES)", status: "Canceled", date: "20240828" },
-    { declaration: "C 59637-2024-NKbs", quantity: "150.0000", hsCode: "40112011", itemName: "275/70R22.5 (POWERTRAC HIGHWAY TIRES)", status: "Pending", date: "20240825" },
-    { declaration: "C 59637-2024-NKa", quantity: "300.5000", hsCode: "40112012", itemName: "295/75R22.5 (POWERTRAC OFFROAD TIRES)", status: "Uploaded", date: "20240826" },
-    { declaration: "C 59637-2024-NKp", quantity: "200.2500", hsCode: "40112013", itemName: "385/65R22.5 (POWERTRAC TRUCK TIRES)", status: "Uploaded", date: "20240827" },
-    { declaration: "C 59637-2024-NKl", quantity: "120.0000", hsCode: "40112014", itemName: "225/75R16 (POWERTRAC SUV TIRES)", status: "Canceled", date: "20240828" },
-    { declaration: "C 59637-2024-NKm", quantity: "150.0000", hsCode: "40112011", itemName: "275/70R22.5 (POWERTRAC HIGHWAY TIRES)", status: "Pending", date: "20240825" },
-    { declaration: "C 59637-2024-NKn", quantity: "300.5000", hsCode: "40112012", itemName: "295/75R22.5 (POWERTRAC OFFROAD TIRES)", status: "Uploaded", date: "20240826" },
-    { declaration: "C 59637-2024-NKz", quantity: "200.2500", hsCode: "40112013", itemName: "385/65R22.5 (POWERTRAC TRUCK TIRES)", status: "Uploaded", date: "20240827" },
-    { declaration: "C 59637-2024-NKx", quantity: "120.0000", hsCode: "40112014", itemName: "225/75R16 (POWERTRAC SUV TIRES)", status: "Canceled", date: "20240828" },
-];
-
 const TABS = [
     { label: "All Items", value: "all" },
-    { label: "Successful Items", value: "uploaded" },
+    { label: "Uploaded Items", value: "uploaded" },
     { label: "Pending Items", value: "pending" },
-    { label: "Failed Items", value: "canceled" },
 ];
 
 const ItemsTable = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [selectedTab, setSelectedTab] = useState("all");
     const [searchQuery, setSearchQuery] = useState("");
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const fetchItems = async () => {
+        try {
+            const response = await requestHandler({
+                method: "GET",
+                route: "/Items/AllItems",
+            });
+
+            console.log(response);
+            setItems(response);
+            setSuccess("Items fetched successfully.");
+            setError("");
+        } catch (err) {
+            setError(`Error fetching items: ${err.message}`);
+            setSuccess("");
+        }
+    };
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
 
     const startIndex = (currentPage - 1) * ROWS_PER_PAGE;
     const endIndex = startIndex + ROWS_PER_PAGE;
 
-    const filteredRows = TABLE_ROWS.filter((row) => {
-        const matchesTab = selectedTab === "all" || row.status.toLowerCase() === selectedTab;
-        const matchesSearch = row.hsCode.includes(searchQuery);
+    const filteredRows = items.filter((row) => {
+        const matchesTab =
+            selectedTab === "all" ||
+            (selectedTab === "uploaded" && row.syncStatus === "Posted") ||
+            (selectedTab === "pending" && row.syncStatus !== "Posted");
+        const matchesSearch = row.itemCd.includes(searchQuery);
         return matchesTab && matchesSearch;
     });
 
@@ -99,21 +107,17 @@ const ItemsTable = () => {
                                         backgroundColor: selectedTab === value
                                             ? value === "all" ? "#2296f2" :
                                                 value === "uploaded" ? "#276437" :
-                                                    value === "pending" ? "#f89a3c" :
-                                                        value === "canceled" ? "#c0302e" : "transparent"
+                                                    value === "pending" ? "#f89a3c" : "transparent"
                                             : "transparent",
                                         color: selectedTab === value
                                             ? "#fff"
                                             : value === "all" ? "#2296f2" :
                                                 value === "uploaded" ? "#276437" :
-                                                    value === "pending" ? "#f89a3c" :
-                                                        value === "canceled" ? "#c0302e" : "#000",
-                                        border: `1px solid ${
-                                            value === "all" ? "#2296f2" :
+                                                    value === "pending" ? "#f89a3c" : "#000",
+                                        border: `1px solid ${value === "all" ? "#2296f2" :
                                                 value === "uploaded" ? "#276437" :
-                                                    value === "pending" ? "#f89a3c" :
-                                                        value === "canceled" ? "#c0302e" : "transparent"
-                                        }`,
+                                                    value === "pending" ? "#f89a3c" : "transparent"
+                                            }`,
                                     }}
                                     onClick={() => handleTabChange(value)}
                                 >
@@ -138,34 +142,34 @@ const ItemsTable = () => {
             <CardBody className="px-0 table-container">
                 <table className="w-full min-w-max table-auto text-left">
                     <thead>
-                    <tr>
-                        {TABLE_HEAD.map((head) => (
-                            <th key={head} className="p-4 bg-blue-gray-50/50">
-                                <Typography variant="small" color="blue-gray" className="font-normal leading-none opacity-70">
-                                    {head}
-                                </Typography>
-                            </th>
-                        ))}
-                    </tr>
+                        <tr>
+                            {TABLE_HEAD.map((head) => (
+                                <th key={head} className="p-4 bg-blue-gray-50/50">
+                                    <Typography variant="small" color="blue-gray" className="font-normal leading-none opacity-70">
+                                        {head}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
                     </thead>
                     <tbody>
-                    {currentRows.map((row) => (
-                        <tr key={row.declaration} className="table-row-hover">
-                            <td className="p-4 text-blue-gray-700">{row.declaration}</td>
-                            <td className="p-4 text-blue-gray-700">{row.quantity}</td>
-                            <td className="p-4 text-blue-gray-700">{row.hsCode}</td>
-                            <td className="p-4 text-blue-gray-700">{row.itemName}</td>
-                            <td className="p-4">
-                                <Chip
-                                    size="sm"
-                                    variant="ghost"
-                                    value={row.status}
-                                    color={row.status === "Pending" ? "amber" : row.status === "Canceled" ? "red" : "green"}
-                                />
-                            </td>
-                            <td className="p-4 text-blue-gray-700">{row.date}</td>
-                        </tr>
-                    ))}
+                        {currentRows.map((row) => (
+                            <tr key={row.itemCd} className="table-row-hover">
+                                <td className="p-4 text-blue-gray-700">{row.itemClsCd}</td>
+                                <td className="p-4 text-blue-gray-700">{row.rsdQty}</td>
+                                <td className="p-4 text-blue-gray-700">{row.itemClsCd}</td>
+                                <td className="p-4 text-blue-gray-700">{row.itemNm}</td>
+                                <td className="p-4">
+                                    <Chip
+                                        size="sm"
+                                        variant="ghost"
+                                        value={row.syncStatus === "Posted" ? "Uploaded Item" : "Pending Item"}
+                                        color={row.syncStatus === "Posted" ? "green" : "amber"}
+                                    />
+                                </td>
+                                <td className="p-4 text-blue-gray-700">{new Date().toLocaleDateString()}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </CardBody>
